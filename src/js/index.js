4c41671499a31e6e2017f8521b0f0505f73ceda5
AmaralLuan <luanalfa97@gmail.com>
2020-10-06 11:43:01 -0300

    api

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,18 @@
-// Global app controller
-import x from './test';
+// const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
+// FOR RECIPES.js ===  const res = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`);
 
-console.log(`${x}`);
+import axios from 'axios';
+
+// Global app controller
+async function getResults(query) {
+    try {
+        const proxy = 'https://cors-anywhere.herokuapp.com/';
+        const res = await axios(`${proxy}https://forkify-api.herokuapp.com/api/search?q=${query}`);
+        const recipes = res.data.recipes;
+        console.log(recipes);
+    } catch (error) {
+        alert(error);
+    }
+        
+}
+getResults('chicken');
